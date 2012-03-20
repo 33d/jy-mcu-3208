b61206992bd0fbad6fc71dbfa1ea7cb4b58d11e9
Damien <damien@sail>
2012-03-20 22:41:08 +1030

    Get rid of the test pattern at the start

--- src/display.cpp
+++ src/display.cpp
@@ -11,8 +11,6 @@
 Display::Display(const Font& font)  : font(font) {
     ::HTpinsetup();
     ::HTsetup();
-
-    for (uint8_t i=0;i<32;i++) HTleds[i]=0b01010101<<(i%2);  HTsendscreen();
 }
 
 void Display::show(const char* text, Effect& effect) {
